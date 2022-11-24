5dccf162874edcaeb572da84a40e3ec8b92f51c0
Alex Kostenko <0984181760ss@gmail.com>
2022-11-24 10:49:07 +0200

    add main structure

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Body, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { UserDto } from '../users/dto/user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() user) {
+    console.log('req.user', user);
+
+    return await this.authService.login(user);
+  }
+
+  @Post('signup')
+  async signUp(@Body() user: UserDto) {
+    return await this.authService.create(user);
+  }
+}
